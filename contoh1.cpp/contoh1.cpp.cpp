07359aa80a2ef0fbef70d0b9032be8b96f7277d2
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-06 15:05:35 +0700

    commit 11 input pointer deference pMhs dan arrow operator

--- contoh1.cpp/contoh1.cpp.cpp
+++ contoh1.cpp/contoh1.cpp.cpp
@@ -17,4 +17,9 @@ int main() {
 	mahasiswa& refMhs = mhs; //pointer reference refmhs
 	refMhs.nim = 2; // member access operator
 	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer deference pMhs
+	pMhs->nim = 3; // arrow operator
+	pMhs->showNim();
+	return 0;
 }
